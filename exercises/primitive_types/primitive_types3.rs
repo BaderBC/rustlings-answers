7bcea5a10a6b97c606b86119873d414b23bac7a4
Bartłomiej Strama <kontakt@bstrama.com>
2023-02-12 15:47:04 +0100

    primitive_types completed

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,13 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    //let a: Vec<i32> = [1, 2].to_vec();
+    //let a = vec![42.0, 21.37];
+    let a = ["Are we there yet?"; 404];
+
+
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
